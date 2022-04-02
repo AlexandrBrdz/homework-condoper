2668fce943145700265bbdb52b0aeab0a0d643c5
Александр Бордюжа <a.n.brdz@gmail.com>
2022-04-02 13:58:50 +0300

    третий комит

--- src/ru/scypro/Main.java
+++ src/ru/scypro/Main.java
@@ -3,6 +3,65 @@ package ru.scypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задача 1
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        char space = 32;
+        System.out.println(space);
+
+        // Задача 2
+
+        int and = 0;
+        int clientDeviceYear = 2014;
+        if (and == 0)
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        else if  (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        System.out.println(space);
+
+            // Задача 3
+
+            int year = 2028;
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+
+
+        }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
